56a59b0b1a8afa97b8bf969a37bfafbe3fe685a1
yangcheng01 <yangcheng01@mail.jj.cn>
2022-01-27 14:39:27 +0800

    蔷薇少女大于图片减大小到1MB下

--- py_scripts/image_compress.py
+++ py_scripts/image_compress.py
@@ -1,3 +1,4 @@
+from statistics import quantiles
 import sys, os
 import traverse_dir_files
 from shutil import copy2
@@ -19,16 +20,48 @@ def get_image_parent_dir(image_path):
 
     return parent_name, parent_dir
 
-def compress_one_file_internal(file_path, save_path):
+def get_save_path(file_path, num):
+    parent_name, parent_dir = get_image_parent_dir(file_path)
+    save_dir = parent_dir + "\\" + parent_name + "_compress"
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
 
-    if os.stat(file_path).st_size <= 1024*1024:
-        copy2(file_path, save_path)
+    return save_dir + "\\" + str(num) + file_path[-4:]
+
+def convert_png_to_jpg(file_path, num):
+    if file_path.endswith(".png"):
+        image = Image.open(file_path).convert('RGB')
+        save_path = get_save_path(file_path[:-4] + ".jpg", num)
+        image.save(save_path)
+        return True, save_path
+    else:
+        return False, file_path
+
+def compress_one_file_internal(file_path, num):
+
+    result, converted_path = convert_png_to_jpg(file_path, num)
+
+    if result:
+        save_path = converted_path
+    else:
+        save_path = get_save_path(converted_path, num)
+
+    if os.stat(converted_path).st_size <= 1024*1024:
+        
+        if file_path.endswith(".png"):
+            return
+
+        copy2(converted_path, save_path)
         return
 
-    image = Image.open(file_path)
-    image = image.resize((int(2*image.width/3), int(2*image.height/3)), Image.ANTIALIAS)
+    image = Image.open(converted_path)
+    #image = image.resize((int(image.width/3), int(image.height/3)), Image.ANTIALIAS)
+    image.save(save_path, optimize=True)
 
-    image.save(save_path, optimize=True, quality=50)
+    if os.stat(save_path).st_size > 1024*1024:
+        image.save(save_path, optimize=True, quality=40)
+
+    assert(os.stat(save_path).st_size < 1024*1024)
 
 def compress_one_file(file_path, num):
 
@@ -36,14 +69,7 @@ def compress_one_file(file_path, num):
         not file_path.endswith(".png"):
         return
 
-    parent_name, parent_dir = get_image_parent_dir(file_path)
-    save_dir = parent_dir + "\\" + parent_name + "_compress"
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
-
-    save_path = save_dir + "\\" + str(num) + file_path[-4:]
-
-    compress_one_file_internal(file_path, save_path)
+    compress_one_file_internal(file_path, num)
 
 def main(argv):
     traverse_dir_files.traverse(argv[0], compress_one_file)
