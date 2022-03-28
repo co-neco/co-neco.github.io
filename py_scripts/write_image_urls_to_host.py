b9d81cec07cbc0ec144b01fb6b1447f71fb98736
yangcheng01 <yangcheng01@mail.jj.cn>
2022-03-28 21:05:47 +0800

    刀剑神域的图片源转到自己买的oss

--- py_scripts/write_image_urls_to_host.py
+++ py_scripts/write_image_urls_to_host.py
@@ -10,7 +10,7 @@ def get_cur_python_file_path():
 
 def output_image_urls(image_dir, host_image_dir_name):
 
-    host_dir_root = "https://conecoy.cn/image_hosts"
+    host_dir_root = "https://image-hosts.oss-cn-chengdu.aliyuncs.com"
     image_url_str = ""
     image_url_index = 0
 
